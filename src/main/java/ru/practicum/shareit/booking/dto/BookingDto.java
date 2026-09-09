6216f9036a1fb354cad952018574b33f4748ec98
Nikita <panzer278@yandex.ru>
2026-09-10 02:42:42 +0300

    feat: add user and item controllers

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.booking.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.booking.BookingStatus;
 
 import java.time.LocalDateTime;
--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,13 +1,7 @@
 package ru.practicum.shareit.request;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 
 import java.util.List;
